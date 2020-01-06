ee60fe6b231c116cf2105c3e4cb553c97987ab63
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-06 11:49:13 +0000

    Update Remove-DataSource
    Update Remove-DataSource function to remove custom output and only return raw outcome

--- Public/Remove-DataSource.ps1
+++ Public/Remove-DataSource.ps1
@@ -4,7 +4,7 @@ function Remove-DataSource {
     Remove data-source
 
     .DESCRIPTION
-    Remove a data-source
+    Remove a data-source from JBoss web-application server
 
     .PARAMETER Path
     The path parameter corresponds to the path to the JBoss client.
@@ -12,17 +12,26 @@ function Remove-DataSource {
     .PARAMETER Controller
     The controller parameter corresponds to the hostname and port of the JBoss host.
 
+    .PARAMETER Credentials
+    The optional credentials parameter correspond to the credentials of the account to use to connect to JBoss.
+
     .PARAMETER DataSource
     The data-source parameter corresponds to the name of the data source to remove.
 
-    .PARAMETER Credentials
-    The optional credentials parameter correspond to the credentials of the account to use to connect to JBoss.
+    .INPUTS
+    System.String. You can pipe the data-source name to Remove-DataSource.
+
+    .OUTPUTS
+    System.String. Remove-DataSource returns the raw output of the JBoss client command.
 
     .NOTES
     File name:      Remove-DataSource.ps1
     Author:         Florian Carrier
     Creation date:  19/12/2019
-    Last modified:  19/12/2019
+    Last modified:  06/01/2020
+
+    .LINK
+    Invoke-JBossClient
   #>
   [CmdletBinding (
     SupportsShouldProcess = $true
@@ -56,7 +65,9 @@ function Remove-DataSource {
     [Parameter (
       Position    = 4,
       Mandatory   = $true,
-      HelpMessage = "Name of the data source to be removed"
+      HelpMessage = "Name of the data source to be removed",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
     )]
     [ValidateNotNUllOrEmpty ()]
     [String]
@@ -67,30 +78,14 @@ function Remove-DataSource {
     Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
   }
   Process {
-    Write-Log -Type "INFO" -Object "Removing $DataSource data-source"
+    Write-Log -Type "DEBUG" -Object "Removing $DataSource data-source"
     # Define JBoss client command
     $Command = "/subsystem=datasources/data-source=""$DataSource"":remove()"
     # Execute command
     if ($PSBoundParameters.ContainsKey("Credentials")) {
-      $RemoveDataSource = Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command -Credentials $Credentials
+      Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command -Credentials $Credentials
     } else {
-      $RemoveDataSource = Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command
-    }
-    # Check outcome
-    # /!\ Special parsing to account for failure due to missing data-source
-    # If operation fails
-    if (Select-String -InputObject $RemoveDataSource -Pattern '"outcome" => "failed"' -SimpleMatch -Quiet) {
-      # If resource already exists
-      if (Select-String -InputObject $RemoveDataSource -Pattern '"failure-description" => "(.|\t|\n|\r)*not found"' -Quiet) {
-        Write-Log -Type "WARN" -Object "$DataSource data-source was not found"
-      } else {
-        Write-Log -Type "DEBUG" -Object $RemoveDataSource
-        Write-Log -Type "ERROR" -Object "$DataSource data-source could not be unregistered" -ExitCode 1
-      }
-    }
-    # If no errors are detected
-    elseif (Select-String -InputObject $RemoveDataSource -Pattern '"outcome" => "success"' -SimpleMatch -Quiet) {
-      Write-Log -Type "CHECK" -Object "$DataSource data-source was successfully unregistered"
+      Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command
     }
   }
 }
