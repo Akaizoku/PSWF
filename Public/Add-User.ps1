c3a0badb770cb4851580bde0387606f4c8c540f0
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-08 16:25:31 +0000

    Update Add-User.ps1

--- Public/Add-User.ps1
+++ Public/Add-User.ps1
@@ -4,7 +4,7 @@ function Add-User {
     Add user
 
     .DESCRIPTION
-    Add an user to the WildFly management interface
+    Add a user to a JBoss instance
 
     .PARAMETER Path
     The path parameter corresponds to the path to the add-user script.
@@ -21,11 +21,17 @@ function Add-User {
     .PARAMETER UserGroup
     The optional user group parameter corresponds to the name of the user group to assign the user to.
 
+    .INPUTS
+    System.Management.Automation.PSCredential. You can pipe the credentials of the user to Add-User.
+
+    .OUTPUTS
+    System.String. Add-User returns the raw output of the add-user script.
+
     .NOTES
     File name:      Add-User.ps1
     Author:         Florian Carrier
     Creation date:  15/10/2019
-    Last modified:  17/12/2019
+    Last modified:  07/01/2020
     TODO            Check JBoss client script type (extension)
   #>
   [CmdletBinding (
@@ -43,13 +49,15 @@ function Add-User {
     [Parameter (
       Position    = 2,
       Mandatory   = $true,
-      HelpMessage = "Credentials of the user"
+      HelpMessage = "Credentials of the user",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
     )]
     [ValidateNotNullOrEmpty ()]
     [System.Management.Automation.PSCredential]
     $Credentials,
     [Parameter (
-      Position    = 4,
+      Position    = 3,
       Mandatory   = $true,
       HelpMessage = "Type of user"
     )]
@@ -60,7 +68,7 @@ function Add-User {
     [String]
     $Type,
     [Parameter (
-      Position    = 5,
+      Position    = 4,
       Mandatory   = $false,
       HelpMessage = "User group in which to add the user"
     )]
@@ -80,7 +88,7 @@ function Add-User {
     }
   }
   Process {
-    Write-Log -Type "INFO" -Object "Add $UserType user $($Credentials.UserName)"
+    Write-Log -Type "DEBUG" -Object "Add $UserType user $($Credentials.UserName)"
     # Define JBoss client command
     $AddUserCommand = "& ""$Path"" --user ""$($Credentials.UserName)"" --password ""$($Credentials.GetNetworkCredential().Password)"" --realm ""$Realm"" --silent"
     # Add user group if applicable
@@ -89,7 +97,6 @@ function Add-User {
     }
     Write-Log -Type "DEBUG" -Object $AddUserCommand -Obfuscate $Credentials.GetNetworkCredential().Password
     # Execute add user command
-    Invoke-Expression -Command $AddUserCommand
-    Write-Log -Type "CHECK" -Object "User $($Credentials.UserName) successfully created"
+    Invoke-Expression -Command $AddUserCommand | Out-String
   }
 }
