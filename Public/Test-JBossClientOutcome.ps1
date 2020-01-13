92434f0b2de9c3abaebc5032a6ac9256e4ed4b48
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-13 10:07:55 +0000

    Create Test-JBossClientOutcome.ps1

---
+++ Public/Test-JBossClientOutcome.ps1
@@ -0,0 +1,45 @@
+function Test-JBossClientOutcome {
+  <#
+    .SYNOPSIS
+    Test JBoss client outcome
+
+    .DESCRIPTION
+    Check the outcome of a JBoss client operation
+
+    .PARAMETER Log
+    The log parameter corresponds to the output of the JBoss client.
+
+    .NOTES
+    File name:      Test-JBossClientOutcome.ps1
+    Author:         Florian Carrier
+    Creation date:  10/01/2020
+    Last modified:  10/01/2020
+  #>
+  [CmdletBinding()]
+  Param (
+    [Parameter (
+      Position    = 1,
+      Mandatory   = $true,
+      HelpMessage = "JBoss client command output log",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [ValidateNotNullOrEmpty()]
+    [Object]
+    $Log
+  )
+  Begin {
+    # Get global preference variables
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+  }
+  Process {
+    # Check JBoss client operation outcome
+    if (Select-String -InputObject $Log -Pattern '"outcome" => "success"' -SimpleMatch -Quiet) {
+      # If outcome is successfull
+      return $true
+    } else {
+      # If outcome is failed or an error occured
+      return $false
+    }
+  }
+}
