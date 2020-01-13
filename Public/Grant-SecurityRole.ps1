9513e4e2911fcc4a3e2203638ea30fc48e4667a1
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-13 10:07:40 +0000

    Update Grant-SecurityRole.ps1
    Remove custom output and add credentials parameter

--- Public/Grant-SecurityRole.ps1
+++ Public/Grant-SecurityRole.ps1
@@ -9,11 +9,8 @@ function Grant-SecurityRole {
     .PARAMETER Path
     The path parameter corresponds to the path to the JBoss client.
 
-    .PARAMETER Hostname
-    The hostname parameter corresponds to the name of the JBoss host.
-
-    .PARAMETER Port
-    The port parameter corresponds to the port of the management console.
+    .PARAMETER Controller
+    The controller parameter corresponds to the hostname and port of the JBoss host.
 
     .PARAMETER Role
     The role parameter corresponds to the name of the security role to be granted.
@@ -25,7 +22,7 @@ function Grant-SecurityRole {
     File name:      Grant-SecurityRole.ps1
     Author:         Florian Carrier
     Creation date:  21/10/2019
-    Last modified:  13/12/2019
+    Last modified:  09/01/2020
   #>
   [CmdletBinding (
     SupportsShouldProcess = $true
@@ -50,16 +47,30 @@ function Grant-SecurityRole {
     $Controller,
     [Parameter (
       Position    = 3,
+      Mandatory   = $false,
+      HelpMessage = "JBoss client user credentials",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [System.Management.Automation.PSCredential]
+    $Credentials,
+    [Parameter (
+      Position    = 4,
       Mandatory   = $true,
-      HelpMessage = "Name of the role"
+      HelpMessage = "Name of the role",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
     )]
     [ValidateNotNullOrEmpty ()]
     [String]
     $Role,
     [Parameter (
-      Position    = 3,
+      Position    = 5,
       Mandatory   = $true,
-      HelpMessage = "Name of the user group"
+      HelpMessage = "Name of the user group",
+      ValueFromPipeline               = $true,
+      ValueFromPipelineByPropertyName = $true
     )]
     [ValidateNotNullOrEmpty ()]
     [String]
@@ -70,11 +81,13 @@ function Grant-SecurityRole {
     Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
   }
   Process {
-    Write-Log -Type "INFO" -Object "Grant $Role role to user group $UserGroup"
     # Define command
     $Command = "/core-service=management/access=authorization/role-mapping=$Role/include=group-$($UserGroup):add(name=$UserGroup,type=GROUP)"
     # Execute command
-    $GrantRole = Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command -Redirect
-    Assert-JBossCliCmdOutcome -Log $GrantRole -Object "$Role role" -Verb "grant"
+    if ($PSBoundParameters.ContainsKey("Credentials")) {
+      Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command -Credentials $Credentials
+    } else {
+      Invoke-JBossClient -Path $Path -Controller $Controller -Command $Command
+    }
   }
 }
